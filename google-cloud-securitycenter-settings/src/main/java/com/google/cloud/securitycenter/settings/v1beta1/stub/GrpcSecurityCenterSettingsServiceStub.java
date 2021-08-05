13c63bd28ed610671833e59801f398704843e079
yoshi-automation <yoshi-automation@google.com>
2021-08-05 01:39:15 -0700

    chore: release gapic-generator-java v1.0.17
    Committer: @miraleung
    PiperOrigin-RevId: 388499329
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Aug 3 11:12:29 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: bb0a090d9204110042ab5dee2ce9e06e8071ce54
    Source-Link: https://github.com/googleapis/googleapis/commit/bb0a090d9204110042ab5dee2ce9e06e8071ce54

--- google-cloud-securitycenter-settings/src/main/java/com/google/cloud/securitycenter/settings/v1beta1/stub/GrpcSecurityCenterSettingsServiceStub.java
+++ google-cloud-securitycenter-settings/src/main/java/com/google/cloud/securitycenter/settings/v1beta1/stub/GrpcSecurityCenterSettingsServiceStub.java
@@ -629,7 +629,13 @@ public class GrpcSecurityCenterSettingsServiceStub extends SecurityCenterSetting
 
   @Override
   public final void close() {
-    shutdown();
+    try {
+      backgroundResources.close();
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new IllegalStateException("Failed to close resource", e);
+    }
   }
 
   @Override
